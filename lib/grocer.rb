429b6c76926eb4a8d8bc449073f6535b04104b16
Arpita Dutta <duttaarpita22@gmail.com>
2020-08-06 17:04:36 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -12,28 +12,18 @@ def find_item_by_name_in_collection(name, collection)
 end
 
 def consolidate_cart(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This returns a new Array that represents the cart. Don't merely
-  # change `cart` (i.e. mutate) it. It's easier to return a new thing.
-  def consolidate_cart(cart)
   hash = {}
-  #O(n)
   cart.each do |cart_item|
     key = cart_item
     value  = hash[key]
     if  value == 1
-      #O(1)
       hash[key] = value + 1
     else
       hash[key] = 1
     end
   end
   arr = []
-  #O(n)
   hash.each do |key, value|
-    #data = key
-    #data[:count] = value
     data = {
       :item => key[:item],
       :price => key[:price],
